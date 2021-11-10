a2f74d363821821e7b5f73804a5d369834ba7323
ibntofajjal <joynal@bintofajjal.info>
2021-11-10 12:46:51 +0600

    uncomplete the site :) ! Hey examiner will pray for me so that I can go to SCIC and understand the concepts

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
-// Import Express and MongoDB included 'dotenv'
 const express = require("express");
-const { MongoClient } = require("mongodb");
-const cors = require("cors");
-const ObjectId = require("mongodb").ObjectId;
-require("dotenv").config();
-
-// Middleware
-const port = process.env.PORT || 5000;
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+const { MongoClient } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nlpea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("travelSpot");
+    const spotsCollection = database.collection("spots");
+    const bookingsCollection = database.collection("bookings");
+
+    await client.connect();
+
+    // Add Spots/Services
+    app.post("/addSpot", async (req, res) => {
+      const result = await spotsCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+    // Get All Sports/Services
+    app.get("/services", async (req, res) => {
+      const result = await spotsCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    // Get Single Product
+    app.get("/singleProduct/:id", async (req, res) => {
+      const result = await spotsCollection
+        .find({ _id: ObjectId(req.params.id) })
+        .toArray();
+      res.send(result[0]);
+    });
+
+    // Confirm Order
+    app.post("/confirmOder", async (req, res) => {
+      const result = await bookingsCollection.insertOne(req.body);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Checking The Server is running or not :)
 app.get("/", (req, res) => {
